ba851ba1bea4b0171ee847f10f600a6511d464d5
Richardjona <richjo4526@gmail.com>
2022-12-09 22:56:13 +0100

    added users login

--- airline/Users/views.py
+++ airline/Users/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 # Create your views here.
 def index(request):
-    pass
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse("login"))
+    else: 
+        render(request, "users/user.html")
+
+def login_view(request):
+    if not request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "users/login.html", {
+                "message": "invalid credentials"
+            })
+
+
+def logout_view(request):
+    logout(request)
+    return render(request, "users/login.html", {
+        "message": "You are loggedout"
+    })
--- airline/airline/urls.py
+++ airline/airline/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("flights/", include("flights.urls")),
-    path("Users/", include("Users.urls"))
+    path("users/", include("users.urls"))
 ]
